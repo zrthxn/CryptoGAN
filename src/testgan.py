e4ab82a64a1dbe61210d4db1691ce6dc08753070
Alisamar Husain <zrthxn@gmail.com>
2020-09-30 19:18:28 +0530

    Reduce files

--- src/testgan.py
+++ src/testgan.py
@@ -3,32 +3,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Generator(nn.Module):
-    def __init__(self):
-        super(Generator, self).__init__()
+  def __init__(self):
+    super(Generator, self).__init__()
+    self.inlayer = nn.Linear(10, 20)
 
-        self.inlayer = nn.Linear(10, 20)
+  def forward(self, *inputs):
+    outputs = torch.Tensor()
 
-    def forward(self, *inputs):
-        outputs = torch.Tensor()
+    for _x in inputs:
+      outputs.append(self.flow(_x))
+    
+    return outputs
 
-        for _x in inputs:
-            outputs.append(
-                self.flow(_x)
-            )
-        
-        return outputs
-
-    def flow(self, _x):
-        _x = self.inlayer(_x)
-        _x = F.relu(_x)
+  def flow(self, _x):
+    _x = self.inlayer(_x)
+    _x = F.relu(_x)
 
 
 class Discriminator(nn.Module):
-    def __init__(self):
-        super(Discriminator, self).__init__()
-
-        self.inlayer = nn.Linear(20, 10)
-    
-    def forward(self, *inputs):
-        pass
+  def __init__(self):
+    super(Discriminator, self).__init__()
+    self.inlayer = nn.Linear(20, 10)
+  
+  def forward(self, *inputs):
+    pass
     
