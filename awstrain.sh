6ba4961169eeeca0709066becc1a43547938bc02
Alisamar Husain <zrthxn@gmail.com>
2020-10-07 16:41:00 +0530

    PDFTEX

---
+++ awstrain.sh
@@ -0,0 +1,25 @@
+echo "This script will send the desired file to AWS."
+
+read -p "AWS EC2 IP/Host: " ec2ip
+read -p "AWS EC2 User: " ec2usr
+read -p "File Name (src/): " file
+
+# pemfile = '/mnt/c/Users/User/Desktop/zxaws.pem'
+
+sudo scp -i /mnt/c/Users/User/Desktop/zxaws.pem src/$file $ec2usr@$ec2ip:~/$file
+
+# Server Area
+sudo ssh -l $ec2usr@$ec2ip -i /mnt/c/Users/User/Desktop/zxaws.pem
+
+read -p "Start training? (Y/N) " start
+
+if [$start == 'Y']
+then
+  python3 $file
+fi
+
+echo "Quitting"
+logout
+# End Server Area
+
+echo "End"
